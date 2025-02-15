5c1e9477430ac21945b84e78dc87e54b30ff8d1b
parham <p1377parsa@gmail.com>
2025-02-16 00:47:02 +0330

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
@@ -95,6 +96,7 @@ export default function Home() {
           />
           Go to nextjs.org →
         </a>
+        <Button className=" bg-red-500">Hello world</Button>
       </footer>
     </div>
   );
